1944d40ed43161ef05a7f0232c2ee10488bb9f07
colla69 <colla69@gmail.com>
2019-05-28 08:54:05 +0200

    new structure + test audio ducking + code clean

--- __init__.py
+++ __init__.py
@@ -4,15 +4,19 @@ from adapt.intent import IntentBuilder
 from mycroft.skills.core import MycroftSkill, intent_handler
 from mycroft.util.log import LOG
 import pynput
-
+from alsaaudio import Mixer, mixers as alsa_mixers
 keyboard = pynput.keyboard.Controller()
+
 
 __author__ = 'colla69'
 
+
 browserCMD = "sensible-browser"
 allCMD = "sensible-browser & skypeforlinux & whatsie &"
-pyCharmCMD = "env BAMF_DESKTOP_FILE_HINT=/var/lib/snapd/desktop/applications/pycharm-professional_pycharm-professional.desktop /snap/bin/pycharm-professional  &"
-ideaCMD = "env BAMF_DESKTOP_FILE_HINT=/var/lib/snapd/desktop/applications/intellij-idea-ultimate_intellij-idea-ultimate.desktop /snap/bin/intellij-idea-ultimate  &"
+pyCharmCMD = "env BAMF_DESKTOP_FILE_HINT=/var/lib/snapd/desktop/applications/pycharm-professional_pycharm" \
+             "-professional.desktop /snap/bin/pycharm-professional  & "
+ideaCMD = "env BAMF_DESKTOP_FILE_HINT=/var/lib/snapd/desktop/applications/intellij-idea-ultimate_intellij-idea" \
+          "-ultimate.desktop /snap/bin/intellij-idea-ultimate  & "
 raiunoCMD = "firefox 'http://webtvhd.com/rai-uno-live.php' "
 
 
@@ -21,45 +25,40 @@ def beamer_screen():
 
 
 def table_screen():
-    os.system(
-        "xrandr --output DVI-I-2 --mode 1920x1080 --output DVI-I-3 --mode 1920x1080  --right-of DVI-I-2 --output HDMI-0 --off")
+    os.system("xrandr --output DVI-I-2 --mode 1920x1080 --output DVI-I-3 --mode 1920x1080  --right-of DVI-I-2 "
+              "--output HDMI-0 --off")
 
 
 class ComputerHelperSkill(MycroftSkill):
 
-    # The constructor of the skill, which calls MycroftSkill's constructor
     def __init__(self):
         super(ComputerHelperSkill, self).__init__(name="TemplateSkill")
         # Initialize working variables used within the skill.
         self.dictation_words = []
-        self.read_vocab("browser.voc")
-        self.read_vocab("pycharm.voc")
-        self.read_vocab("all.voc")
-        self.read_vocab("raiuno.voc")
+        try:
+            # If there are only 1 mixer use that one
+            mixers = alsa_mixers()
+            if len(mixers) == 1:
+                self.mixer = Mixer(mixers[0])
+            else:  # Try using the default mixer
+                self.mixer = Mixer()
+        except Exception:
+            # Retry instanciating the mixer
+            try:
+                self.mixer = Mixer()
+            except Exception as e:
+                self.log.error('Couldn\'t allocate mixer, {}'.format(repr(e)))
 
     def initialize(self):
-        beamer_intent = IntentBuilder("BeamerIntent"). \
-            require("BeamerKeyword").build()
-        self.register_intent(beamer_intent,
-                             self.handle_beamer_intent)
-        table_intent = IntentBuilder("TableIntent"). \
-            require("TableKeyword").build()
-        self.register_intent(table_intent,
-                             self.handle_table_intent)
-
-        self.register_entity_file("chrome.entity")
-
-    def handle_beamer_intent(self, message):
-        beamer_screen()
-        self.speak_dialog("changes.done")
-
-    def handle_table_intent(self, message):
-        table_screen()
-        self.speak_dialog("changes.done")
+        beamer_intent = IntentBuilder("BeamerIntent").require("BeamerKeyword").build()
+        self.register_intent(beamer_intent, self.handle_beamer_intent)
+        table_intent = IntentBuilder("TableIntent").require("TableKeyword").build()
+        self.register_intent(table_intent,self.handle_table_intent)
+        self.add_event('recognizer_loop:record_begin', self.handle_listener_started)
+        self.add_event('recognizer_loop:record_end', self.handle_listener_stopped)
 
     def read_vocab(self, name=""):
-        path = join(dirname(__file__), "vocab", self.lang,
-                    name)
+        path = join(dirname(__file__), "vocab", self.lang, name)
         LOG.info(path)
         with open(path, 'r') as voc_file:
             for line in voc_file.readlines():
@@ -69,9 +68,36 @@ class ComputerHelperSkill(MycroftSkill):
                 for alias in parts[1:]:
                     self.dictation_words.append(alias)
 
-    # @intent_handler(IntentBuilder("OpenerIntent").require("OpenKeyword"))
-    # def handle_opener_intent(self, message):
-    #     self.speak_dialog("choose.app", expect_response=True)
+    ######################################################################
+    # audio ducking
+
+    def handle_listener_started(self, message):
+        #self.mixer.setvolume(10)
+        vol = self.mixer.getvolume()[0]
+        print(vol)
+
+        # if self.get_running():
+        #   volume = self.player.get_media_player().audio_get_volume()
+        #   half = volume // 2
+        #   self.player.get_media_player().audio_set_volume(half)
+
+    def handle_listener_stopped(self, message):
+        pass
+        # if self.get_running():
+        #    volume = self.player.get_media_player().audio_get_volume()
+        #    doubleit = volume * 2
+        #    self.player.get_media_player().audio_set_volume(doubleit)
+
+    ######################################################################
+    # intents
+
+    def handle_beamer_intent(self, message):
+        beamer_screen()
+        self.speak_dialog("changes.done")
+
+    def handle_table_intent(self, message):
+        table_screen()
+        self.speak_dialog("changes.done")
 
     @intent_handler(IntentBuilder("BrowserIntent").require("browser"))
     def handle_browser_intent(self, message):
@@ -88,10 +114,6 @@ class ComputerHelperSkill(MycroftSkill):
     @intent_handler(IntentBuilder("IdeaIntent").require("idea"))
     def handle_idea_intent(self, message):
         execute(ideaCMD)
-
-    @intent_handler(IntentBuilder("RaiUnoIntent").require("raiuno"))
-    def handle_raiuno_intent(self, message):
-        execute(raiunoCMD)
 
     @intent_handler(IntentBuilder("RefreshIntent").require("refresh"))
     def handle_refresh_intent(self, message):
